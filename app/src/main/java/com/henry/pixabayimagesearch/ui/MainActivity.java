d96a7e8f072fd9971f9067b5362189450a0bccad
henry <henry@liuhonghaodeMacBook.local>
2016-12-09 09:08:33 +0800

    Add SearchView

--- app/src/main/java/com/henry/pixabayimagesearch/ui/MainActivity.java
+++ app/src/main/java/com/henry/pixabayimagesearch/ui/MainActivity.java
@@ -1,15 +1,22 @@
 package com.henry.pixabayimagesearch.ui;
 
+import android.app.SearchManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.henry.pixabayimagesearch.Adapter.PixabayListAdapter;
 import com.henry.pixabayimagesearch.App;
@@ -29,8 +36,10 @@ import java.util.ArrayList;
 import uk.co.senab.bitmapcache.BitmapLruCache;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = "PixabayImageSearch";
     private AsyncListView mListView;
     private AsyncGridView mGridView;
+    private SearchView searchView;
     private boolean isGrid = false;
 
     @Override
@@ -39,6 +48,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mListView = (AsyncListView) findViewById(R.id.list);
+        mGridView = (AsyncGridView) findViewById(R.id.grid);
+        updateLayout();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -49,22 +62,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mListView = (AsyncListView) findViewById(R.id.list);
-        mGridView = (AsyncGridView) findViewById(R.id.grid);
-        updateLayout();
-
         BitmapLruCache cache = App.getInstance(this).getBitmapCache();
         PixabayListLoader loader = new PixabayListLoader(cache);
 
         ItemManager.Builder builder = new ItemManager.Builder(loader);
         builder.setPreloadItemsEnabled(true).setPreloadItemsCount(5);
         builder.setThreadPoolSize(4);
+        ItemManager manager = builder.build();
 
-        mListView.setItemManager(builder.build());
-
-        mGridView.setItemManager(builder.build());
-
-        new LoadPatternsListTask().execute();
+        mListView.setItemManager(manager);
+        mGridView.setItemManager(manager);
     }
 
     private void updateLayout() {
@@ -110,23 +117,51 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        if (searchItem != null) {
+            searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+            searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+                @Override
+                public boolean onClose() {
+                    //some operation
+                    Log.d(LOG_TAG, "onClose is clicked");
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                    return false;
+                }
+            });
+            searchView.setOnSearchClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //some operation
+                }
+            });
+            EditText searchPlate = (EditText) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+            searchPlate.setHint("Search");
+            View searchPlateView = searchView.findViewById(android.support.v7.appcompat.R.id.search_plate);
+            searchPlateView.setBackgroundColor(ContextCompat.getColor(this, android.R.color.transparent));
+            // use this method for search process
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    // use this method when query submitted
+                    //Toast.makeText(getApplicationContext(), query, Toast.LENGTH_SHORT).show();
+                    new LoadPatternsListTask().execute();
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    // use this method for auto complete search process
+                    return false;
+                }
+            });
+            SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
         }
-
-        return super.onOptionsItemSelected(item);
+        return super.onCreateOptionsMenu(menu);
     }
+
 }
